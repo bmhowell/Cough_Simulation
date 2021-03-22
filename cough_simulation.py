2c70e439e57c8d683d0f18a8eae83619dd3e2af4
Brian Howell <bhowell@berkeley.edu>
2021-03-22 15:50:13 -0700

    ndarray issues in fdrag

--- cough_simulation.py
+++ cough_simulation.py
@@ -1,8 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-#----------------------------------------------
-# constants
+#---------------- constants --------------------
 h0 = np.array([0, 2, 0]).T      # starting height (m)
 g = np.array([0, -9.81, 0]).T   # gravity (m/s^2)
 time = 4                        # simulation time (s)
@@ -15,11 +14,12 @@ vf = np.array([0, 0, 0]).T      # surrounding fluid velocity (m/s)
 muf = 1.8e-5                    # surrounding fluid viscosity (Pa/s)
 rhoF = 1.225                    # density of air (kg / m^3)
 
-# particle generation
+#---------------- particle generation --------------------
 R_ave = 0.0001                  # average particle radius (m)
 R = []                          # vector containing particle radii
 Mtot = 0                        # track total number of particles
 mi = []                         # mass of each particle (kg)
+Aci = []                        # cross sectional area of each particle (m^2)
 A = 0.9975                      # deviatiatoric constant
 i = 0                           # index
 counter = True                  # counter
@@ -28,7 +28,8 @@ while counter:
     xi = np.random.randint(low=-1000, high=1000, size=1)/1000;
     R.append(R_ave * (1 + A * xi))
     Mtot = Mtot + rho*(4/3)*np.pi*R[-1]**3
-    mi.append(rho*(4/3)*np.pi*R[i]**3)
+    mi.append(rho*(4/3)*np.pi*R[-1]**3)
+    Aci.append(np.pi * R[-1]**2)
     if Mtot >= mTot:
         counter = False
         pTot = i + 1
@@ -37,7 +38,7 @@ while counter:
         print('rMax = ', np.amax(R))
     else:
         i += 1
-
+Aci = np.asarray(Aci)
 R = np.asarray(R).T
 mi = np.asarray(mi).T
 
@@ -79,7 +80,10 @@ for i in range(1):
     print(fGrav.shape)
 
     vf_ = np.outer(vf, ones).T
-    vdiff = np.linalg.norm(vf_ - vi, 2, 1)
+    vdiff = np.linalg.norm(vf - vi, 2, 1)
+    # vdiff2 = np.linalg.norm(vf_-vi, 2, 1)
+
+
 
     Re = (2 * R * rhoF * vdiff / muf).T
 
@@ -89,17 +93,24 @@ for i in range(1):
     cond4 = np.where(np.logical_and(Re > 3e5, Re <= 2e6))[0]
     cond5 = np.where(np.logical_and(Re > 2e6, Re > 1.0))[0]
 
-    print('test = ' , Re[cond1])
-    print('test = ', Re[cond3])
-    if cond1:
-        Cd[cond1] = np.true_divide(24., Re[cond1])
-    print('cond2 ', cond2)
-    if cond2:
-        Cd[cond2] = np.true_divide(24., np.power(Re[cond2], 0.646))
-    if cond3:
-        Cd[cond3] = 0.000366 * np.power(Re[cond3], 0.4275)
-    if cond4:
-        Cd[cond4] = 0.18
+    if len(cond1) > 0:
+        Cd[cond1] = 24. / Re[cond1]
+    if len(cond2) > 0:
+        Cd[cond2] = 24. / np.power(Re[cond2], 0.646)
+    if len(cond3) > 0:
+        Cd[cond3] = 0.5
+    if len(cond4) > 0:
+        Cd[cond4] = 0.000366 * np.power(Re[cond3], 0.4275)
+    if len(cond5) > 0:
+        Cd[cond5] = 0.18
+
+    print('shape Cd: ', Cd.shape)
+    print('shape Aci: ', Aci.shape)
+    print('shape vdiff: ', vdiff.shape)
+    print('shape vi: ', vi.shape)
+    print('shape vf: ', (vi-vf).shape)
+    # fDrag = 0.5 * Cd * rhoF * Aci * vdiff * (vi - vf)
+    # print('fDrag = ', fDrag)
 
 
 
